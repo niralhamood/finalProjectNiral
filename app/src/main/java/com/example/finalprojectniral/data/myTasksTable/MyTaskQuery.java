d3d9a85eaeb29976a1a651a56823c762cb70c8be
class01 <hamoniral@gmail.com>
2025-11-13 14:07:34 +0200

    tdrgthtdh

--- app/src/main/java/com/example/finalprojectniral/data/myTasksTable/MyTaskQuery.java
+++ app/src/main/java/com/example/finalprojectniral/data/myTasksTable/MyTaskQuery.java
@@ -1,29 +1,44 @@
 package com.example.finalprojectniral.data.myTasksTable;
 
+import static android.icu.text.MessagePattern.ArgType.SELECT;
+
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
+
+import kotlinx.coroutines.scheduling.Task;
 
 @Dao
 public interface MyTaskQuery {
 
- قائمة من المهمات * return@*
- @Query("SELECT * FROM MyTask ORDER BY Importance DESC")
- List<MyTask> getAll Tasks(;
- /**
- ارجاع المهمات حسب المستعمل واذا انتهت ام لا ومرتبة تنازليا حسب الاهمية *
- @param userid_p رقم المستعمل
-  * @return
- */
-    @Query"SELECT * FROM MyTask WHERE userld=:userid_p, ORDER BY time DES")
-    List<MyTask> getAl|TaskOrederBy(long userid_p);
-    /**
-     ارجاع المهمات حسب المستعمل واذا انتهت ام لا ومرتبة تنازليا حسب الاهمية *
-     @param userid_p رقم المستعمل
-     *@param isCompleted_ * هل تمت ام لا
-     * @return * قائمة مهمات
-     */
-    @Query(SELECT * FROM MyTask WHERE userld=:userid_p AND isCompleted=:isCompleted_p"+
-            "ORDER BY importance DESC")
-    List<MyTask> getAl|TaskOrederBy(long userid_p,boolean isCompleted_p);
+
+    @Query("SELECT * FROM MyTask ORDER BY Importance DESC")
+    List<MyTask> getAllTasks();
+
+    @Query("SELECT * FROM MyTask WHERE userld=:userid_p, ORDER BY time DES")
+    List<MyTask> getAlLTaskOrederBy(long userid_p);
+
+    @Query(SELECT * FROM MyTask WHERE userld=:userid_p AND isCompleted=:isCompleted_p" + "ORDER BY importance DESC")
+
+    List<MyTask> getAllTaskOrederBy(long userid_p, boolean isCompleted_p);
+
+    @Insert
+    void insertTask(MyTask... tasks);
+
+    @Update
+    void updateTask(MyTask... tasks);
+
+    @Delete
+    void deleteTask(MyTask... tasks);
+
+    @Query("DELETE FROM MyTask WHERE keyId=kid");
+    void deleteTask(long kid);
+
+    @Query("SELECT * FROM MyTask WHERE subjId=key_id" + "ORDER BY importance DESC");
+
+    List<MyTask> getTaskBySubjId(long key_id);
+}
